77f5afec03fe9e7d5c989ee9b52376753fb65def
camilo campos <camilo.camposog@gmail.com>
2025-05-22 12:37:47 -0400

    componente que falta

---
+++ components/SensorsB.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h2>Sensores Bomba B</h2>
+    <p>Esta es la vista de sensores para la Bomba B. (Próximamente se hará modular)</p>
+  </div>
+</template>
+
+<script setup>
+// Aquí irá la lógica específica para la Bomba B en el futuro
+</script>
